fd68b4f046aa4050884cc92a20ae3af561b5c22f
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-10 10:40:53 +0530

    Export all the constant variables

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,8 @@
-
+// Game constants
+export const PADDLE_WIDTH = 10;
+export const PADDLE_HEIGHT = 80;
+export const BALL_SIZE = 14;
+export const PLAYER_X = 20;
+export const AI_X = canvas.width - PADDLE_WIDTH - 20;
+export const PADDLE_SPEED = 7;
+export const BALL_SPEED = 6;
