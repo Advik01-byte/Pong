49f0bc3f021fd594de61b73f481805d5073fd513
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-10 10:47:29 +0530

    Export all the states

--- scripts/states.js
+++ scripts/states.js
@@ -1,1 +1,9 @@
-
+// State
+export let playerY = canvas.height / 2 - PADDLE_HEIGHT / 2;
+export let aiY = canvas.height / 2 - PADDLE_HEIGHT / 2;
+export let ballX = canvas.width / 2 - BALL_SIZE / 2;
+export let ballY = canvas.height / 2 - BALL_SIZE / 2;
+export let ballVelX = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+export let ballVelY = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+export let playerScore = 0;
+export let aiScore = 0;
