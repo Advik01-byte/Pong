81f9a195a0d3e3376db05e0cf0b8222aa0e5a0b6
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-10 10:39:52 +0530

    Ready the file to import the constant variables from constants.js

--- scripts/game.js
+++ scripts/game.js
@@ -3,15 +3,6 @@ import { draw, gameLoop, updateAI, updateBall, resetBall } from './functions.js'
 const canvas = document.getElementById('pongCanvas');
 const ctx = canvas.getContext('2d');
 
-// Game constants
-const PADDLE_WIDTH = 10;
-const PADDLE_HEIGHT = 80;
-const BALL_SIZE = 14;
-const PLAYER_X = 20;
-const AI_X = canvas.width - PADDLE_WIDTH - 20;
-const PADDLE_SPEED = 7;
-const BALL_SPEED = 6;
-
 // State
 let playerY = canvas.height / 2 - PADDLE_HEIGHT / 2;
 let aiY = canvas.height / 2 - PADDLE_HEIGHT / 2;
