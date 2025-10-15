01d2beadbf5d5ccca9a4b2384ef1aa886fcd72e9
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-15 16:10:54 +0530

    Fix an error

--- scripts/states.js
+++ scripts/states.js
@@ -1,12 +1,9 @@
-const canvas = document.getElementById('pongCanvas');
-const ctx = canvas.getContext('2d');
-
 // State
-export let playerY = canvas.height / 2 - PADDLE_HEIGHT / 2;
-export let aiY = canvas.height / 2 - PADDLE_HEIGHT / 2;
-export let ballX = canvas.width / 2 - BALL_SIZE / 2;
-export let ballY = canvas.height / 2 - BALL_SIZE / 2;
-export let ballVelX = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
-export let ballVelY = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
-export let playerScore = 0;
-export let aiScore = 0;
+let playerY = canvas.height / 2 - PADDLE_HEIGHT / 2;
+let aiY = canvas.height / 2 - PADDLE_HEIGHT / 2;
+let ballX = canvas.width / 2 - BALL_SIZE / 2;
+let ballY = canvas.height / 2 - BALL_SIZE / 2;
+let ballVelX = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+let ballVelY = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+let playerScore = 0;
+let aiScore = 0;
