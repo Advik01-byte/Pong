4aad60c13663624e52aa657ff4744e0373f0ffbb
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-15 19:29:19 +0530

    Reload the page after score is reset

--- scripts/states.js
+++ scripts/states.js
@@ -14,4 +14,6 @@ let aiScore = localStorage.getItem('aiScore') || 0;
 resetButton.addEventListener('click', () => {
     localStorage.removeItem('playerScore');
     localStorage.removeItem('aiScore');
+    
+    location.reload();
 });
